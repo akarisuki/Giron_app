5ddb6311440d0bec58d9b84f93334516b9bab938
akarisuki <akarisuki1222@gmail.com>
2022-08-23 20:31:19 +0900

    React組み込み完了

--- src/laravelapp/resources/views/welcome.blade.php
+++ src/laravelapp/resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel</title>
 
@@ -62,6 +63,7 @@
                 margin-bottom: 30px;
             }
         </style>
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -94,7 +96,11 @@
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
+
+                <div id="example"></div>
             </div>
         </div>
+
+        <script src="{{ mix('js/app.js')}}"></script>
     </body>
 </html>
